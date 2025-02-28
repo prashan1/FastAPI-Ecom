87b5e37f1c4fde167c73412a75f3bfbc72f664d8
Prashantdev200 <112601547+Prashantdev200@users.noreply.github.com>
2025-02-28 23:07:02 +0530

    Added pydantic-models, following repository design for db-queries, Dependency Injection for rout handlers

--- backend/app/api/routes/cleanings.py
+++ backend/app/api/routes/cleanings.py
@@ -1,5 +1,11 @@
-from fastapi import APIRouter
 from typing import List
+ 
+from fastapi import APIRouter, Body, Depends  
+from starlette.status import HTTP_201_CREATED  
+ 
+from app.models.cleaning import CleaningCreate, CleaningPublic  
+from app.db.repositories.cleanings import CleaningsRepository  
+from app.api.dependencies.database import get_repository
 
 router = APIRouter()
 
@@ -8,3 +14,11 @@ async def get_all_cleanings() -> List[dict]:
     return [
         {"id": 1, "name": "My house", "cleaning_type": "full_clean", "price_per_hour": 29.99},
     ] 
+    
+@router.post('/', response_model=CleaningPublic, name='cleanings:create-cleaning', status_code=HTTP_201_CREATED)
+async def create_new_cleaning(
+    new_cleaning: CleaningCreate = Body(..., embed=True),
+    cleanings_repo: CleaningsRepository = Depends(get_repository(CleaningsRepository))
+) -> CleaningPublic:
+    new_cleaning = cleanings_repo.create_cleaning(new_cleaning=new_cleaning)
+    return new_cleaning
---
+++ backend/app/models/cleaning.py
@@ -0,0 +1,35 @@
+from enum import Enum
+from typing import Optional
+
+from app.models.core import IDModelMixin, CoreModel
+
+class CleaningType(str, Enum):
+    dust_up = "dust_up"
+    spot_clean = "spot_clean"
+    full_clean = "full_clean"
+    
+    
+class CleaningBase(CoreModel):
+    name: Optional[str]
+    price: Optional[float]
+    description: Optional[str]
+    cleaning_type: Optional[CleaningType] = 'spot_clean'
+
+
+class CleaningCreate(CoreModel):
+    name: str
+    price: float
+    
+    
+class CleaningUpdate(CoreModel):
+    cleaning_type: Optional[CleaningType]
+    
+    
+class CleaningInDB(IDModelMixin, CleaningBase):
+    name: str
+    price: float
+    cleaning_type: CleaningType
+
+
+class CleaningPublic(IDModelMixin, CleaningBase):
+    pass
